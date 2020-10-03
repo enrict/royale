48f109044f66d4b28fc4a31069e05a956930a0ba
enrict <enric_trillo@outlook.com>
2020-10-03 18:54:03 +0100

    updated app to redirect user if logged in

--- src/redux/user/userReducer.js
+++ src/redux/user/userReducer.js
@@ -1,10 +1,12 @@
+import { userActionTypes } from "./userActionTypes";
+
 const initial_state = {
   currentUser: null,
 };
 
 const userReducer = (state = initial_state, action) => {
   switch (action.type) {
-    case "SET_CURRENT_USER":
+    case userActionTypes.SET_CURRENT_USER:
       return {
         ...state,
         currentUser: action.payload,
